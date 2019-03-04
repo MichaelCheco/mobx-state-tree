2cb7ab2060bc851de4b8ee19d30b178ffabe8ba3
Michael <checomichael2@gmail.com>
2019-03-04 18:18:43 -0500

    created our wishlist models and tested them

---
+++ wishlist/src/models/WishList.test.js
@@ -0,0 +1,24 @@
+import { WishListItem, WishList } from './WishList';
+
+it('can create an instance of a model', () => {
+	const item = WishListItem.create({
+		name: 'War and Peace',
+		price: 32.99,
+		// image: '',
+	});
+	expect(item.image).toBe('');
+	expect(item.price).toBe(32.99);
+});
+
+it('can create a wishlist', () => {
+	const list = WishList.create({
+		items: [
+			{
+				name: 'War and Peace',
+				price: 32.99,
+			},
+		],
+	});
+	expect(list.items.length).toBe(1);
+	expect(list.items[0].price).toBe(32.99);
+});
