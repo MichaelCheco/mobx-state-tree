2294d859e106d2d694d182d173766cf0ba4c41d5
Michael <checomichael2@gmail.com>
2019-03-05 14:47:05 -0500

    added snapshots / patches to tests

--- wishlist/src/models/WishList.test.js
+++ wishlist/src/models/WishList.test.js
@@ -1,5 +1,5 @@
 import { WishListItem, WishList } from './WishList';
-
+import { getSnapshot, onSnapshot, onPatch } from 'mobx-state-tree';
 it('can create an instance of a model', () => {
 	const item = WishListItem.create({
 		name: 'War and Peace',
@@ -27,14 +27,42 @@ it('can create a wishlist', () => {
 
 it('can add new items', () => {
 	const list = WishList.create();
-	list.add(
-		WishListItem.create({
-			name: 'Musashi',
-			price: 50.0,
-		})
-	);
+	const states = [];
+	onSnapshot(list, snapshot => {
+		states.push(snapshot);
+	});
+	list.add({
+		name: 'Musashi',
+		price: 50.0,
+	});
 	expect(list.items.length).toBe(1);
 	expect(list.items[0].price).toBe(50.0);
 	list.items[0].changeName('Essentialism');
 	expect(list.items[0].name).toBe('Essentialism');
+
+	expect(getSnapshot(list)).toMatchSnapshot();
+	expect(states).toMatchSnapshot();
+	// expect(getSnapshot(list)).toEqual({
+	// 	items: [
+	// 		{
+	// 			name: 'Essentialism',
+	// 			price: 50.0,
+	// 			image: '',
+	// 		},
+	// 	],
+	// });
+});
+it('can add new items -2', () => {
+	const list = WishList.create();
+	const patches = [];
+	onPatch(list, patch => {
+		patches.push(patch);
+	});
+	list.add({
+		name: 'Musashi',
+		price: 50.0,
+	});
+	list.items[0].changeName('Essentialism');
+
+	expect(patches).toMatchSnapshot();
 });
