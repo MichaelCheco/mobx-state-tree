bea2fc1d4776c06ed57783d8bfde9c1b36d28669
Michael <checomichael2@gmail.com>
2019-03-05 17:52:14 -0500

    persist data using local storage

--- wishlist/src/index.js
+++ wishlist/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { WishList } from './models/WishList';
-const wishList = WishList.create({
+import { onSnapshot } from 'mobx-state-tree';
+let initialState = {
 	items: [
 		{
 			name: 'Paper',
@@ -15,5 +16,13 @@ const wishList = WishList.create({
 			image: '',
 		},
 	],
+};
+if (localStorage.getItem('wishlistapp')) {
+	const json = JSON.parse(localStorage.getItem('wishlistapp'));
+	if (WishList.is(json)) initialState = json;
+}
+const wishList = WishList.create(initialState);
+onSnapshot(wishList, snapshot => {
+	localStorage.setItem('wishlistapp', JSON.stringify(snapshot));
 });
 ReactDOM.render(<App wishList={wishList} />, document.getElementById('root'));
