32d4190cf93f35048f8768835ddd03921c313858
Michael <checomichael2@gmail.com>
2019-03-05 17:59:10 -0500

    added hot reloading

--- wishlist/src/index.js
+++ wishlist/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { WishList } from './models/WishList';
-import { onSnapshot } from 'mobx-state-tree';
+import { onSnapshot, getSnapshot } from 'mobx-state-tree';
 let initialState = {
 	items: [
 		{
@@ -17,12 +17,27 @@ let initialState = {
 		},
 	],
 };
-if (localStorage.getItem('wishlistapp')) {
-	const json = JSON.parse(localStorage.getItem('wishlistapp'));
-	if (WishList.is(json)) initialState = json;
+// if (localStorage.getItem('wishlistapp')) {
+// 	const json = JSON.parse(localStorage.getItem('wishlistapp'));
+// 	if (WishList.is(json)) initialState = json;
+// }
+let wishList = WishList.create(initialState);
+// onSnapshot(wishList, snapshot => {
+// 	localStorage.setItem('wishlistapp', JSON.stringify(snapshot));
+// });
+function renderApp() {
+	ReactDOM.render(<App wishList={wishList} />, document.getElementById('root'));
 }
-const wishList = WishList.create(initialState);
-onSnapshot(wishList, snapshot => {
-	localStorage.setItem('wishlistapp', JSON.stringify(snapshot));
-});
-ReactDOM.render(<App wishList={wishList} />, document.getElementById('root'));
+
+renderApp();
+
+if (module.hot) {
+	module.hot.accept(['./components/App'], () => {
+		renderApp();
+	});
+	module.hot.accept(['./components/App'], () => {
+		const snapshot = getSnapshot(wishList);
+		wishList = wishList.create(snapshot);
+		renderApp();
+	});
+}
